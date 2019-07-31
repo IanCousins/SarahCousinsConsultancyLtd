308665e45c3810b5f913678fe7283be11a62063a
Ian Cousins <ian.cousins@hotmail.co.uk>
2019-07-31 18:36:11 +0100

    menu scroll workinging and added padding to section

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
-window.addEventListener('scroll', function() {
-    $('nav').toggleClass('scrolled', $(this).scrollTop() > 800);
-  });
+// window.addEventListener('scroll', function() {
+//     $('nav').toggleClass('scrolled', $(this).scrollTop() > 800);
+//   });
 
 
-function changeNavColor(){
-    alert("hello");
-    // $('nav').toggleClass('scrolled', $(this).scrollTop() > 800);
-};
+// function changeNavColor(){
+//     alert("hello");
+//     // $('nav').toggleClass('scrolled', $(this).scrollTop() > 800);
+// };
 
 // $(window).scroll(function(){
 // $('nav').toggleClass('scrolled', $(this).scrollTop() > 800);
